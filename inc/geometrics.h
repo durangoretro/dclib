5be7ac7e206ac330bd4635198e44049153318d14
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2022-10-26 14:03:52 +0200

    Geometrics lib

--- inc/geometrics.h
+++ inc/geometrics.h
@@ -13,26 +13,28 @@ extern void __fastcall__ drawFullScreen(unsigned char color);
  * Draw a Pixel on Screen
  * @param x: X Coord in pixels. The x coordinate is from left to Rigth.
  * @param y: Y Coord in pixels. The Y coordinate is from up to Down.
- * @param color: color to use. Check the video.h file for the macros for the 16 colors.
+ * @param color: color to use.
  */
 extern void __cdecl__ drawPixel(unsigned char x, unsigned char y, unsigned char color);
 
 
 /**
+ * Draw a Rectangle on Screen
  * @param x: X Coord in pixels. The x coordinate is from left to Rigth.
  * @param y: Y Coord in pixels. The Y coordinate is from up to Down.
  * @param width: Rectangle width
  * @param height: Rectangle height
- * @param color: color to use. Check the video.h file for the macros for the 16 colors.
+ * @param color: color to use.
  */
 extern void __cdecl__ drawRect(unsigned char x, unsigned char y, unsigned char width, unsigned char height, unsigned char color);
 
 /**
- * @param x1: X Coord in pixels. The x coordinate is from left to Rigth.
- * @param y1: Y Coord in pixels. The Y coordinate is from up to Down.
- * @param x2: Rectangle width
- * @param y2: Rectangle height
- * @param color: color to use. Check the video.h file for the macros for the 16 colors.
+ * Draw a Line on Screen
+ * @param x1: Start X Coord in pixels. The x coordinate is from left to Rigth.
+ * @param y1: Start Y Coord in pixels. The Y coordinate is from up to Down.
+ * @param x2: End X Coord.
+ * @param y2: End Y Coord
+ * @param color: color to use.
  */
 extern void __cdecl__ drawLine(unsigned char x1, unsigned char y1, unsigned char x2, unsigned char y2, unsigned char color);
 
