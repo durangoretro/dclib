e30d96e595555d813bd51731816e08d3e5e500a6
emiliollbb <emiliollbb@gmail.com>
2022-12-30 08:42:37 +0100

    Add glyph lib

--- inc/glyph.h
+++ inc/glyph.h
@@ -1,6 +1,6 @@
 #ifndef GLYPH_H
 #define GLYPH_H
 
-extern void __fastcall__ printBCD(unsigned char x, unsigned char y, long value);
+extern void __fastcall__ printBCD(unsigned char x, unsigned char y, long value, void* font);
 
 #endif
