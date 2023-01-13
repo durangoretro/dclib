47463e6acfacc0234da40300248f60a6f9e718f7
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2023-01-13 10:30:41 +0100

    Glyph. Type letter

--- inc/glyph.h
+++ inc/glyph.h
@@ -1,6 +1,6 @@
 #ifndef GLYPH_H
 #define GLYPH_H
 
-extern void __cdecl__ printBCD(unsigned char x, unsigned char y, void* font, long value);
+extern void __cdecl__ printBCD(unsigned char x, unsigned char y, void* font, unsigned char color, unsigned char paper, long value);
 
 #endif
