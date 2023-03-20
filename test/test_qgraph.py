10dd14c7bf45bae62cd95df8198d02e3a12f86eb
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2023-03-20 12:44:48 +0100

    Test fillScreen

--- test/test_qgraph.py
+++ test/test_qgraph.py
@@ -9,4 +9,6 @@ def internal_fillScreen(color):
     data = bytearray(8192)
     data[0]=color
     dump = durango_testing.run_durango('test_qgraph', data)
-    assert dump[0x6000] == color
+    assert dump[0x5FFF] != color
+    for i in range(8192):
+        assert dump[0x6000+i] == color
