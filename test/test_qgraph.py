6e5ab22b91bf632dc45ba22d1bd75ccd93d948dc
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2023-03-20 11:47:27 +0100

    dclib testing

--- test/test_qgraph.py
+++ test/test_qgraph.py
@@ -1,6 +1,10 @@
 import durango_testing
 
 def test_fillScreen():
+    internal_fillScreen(0x11)
+    
+def internal_fillScreen(color):
     data = bytearray(8192)
+    data[0]=color
     dump = durango_testing.run_durango('test_qgraph', data)
-    assert dump[0x6000] == 0x11
+    assert dump[0x6000] == color
