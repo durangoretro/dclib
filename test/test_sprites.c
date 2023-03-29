e1ee7fa9be6c4302104052cee0f6fdcfc31d3f2b
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2023-03-29 12:07:42 +0200

    Sprites lib

--- test/test_sprites.c
+++ test/test_sprites.c
@@ -1,14 +1,32 @@
 #include <sprites.h>
 #include <psv.h>
 
-sprite player = {0x11, 0x22, 0, 0x33, 0x44, 0};
+char* test_data;
+sprite a, b;
 
 int main() {
-    consoleLogStr("Hello world!\n");
-    consoleLogHex(player.x);
-    consoleLogHex(player.y);
-    consoleLogHex(player.width);
-    consoleLogHex(player.height);
+
+    test_data = 0x8000;
     
-    psvDump();
+    a.x=test_data[0];
+    a.y=test_data[1];
+    a.height=test_data[2];
+    a.width=test_data[3];
+    
+    b.x=test_data[4];
+    b.y=test_data[5];
+    b.height=test_data[6];
+    b.width=test_data[7];
+    
+    consoleLogStr("Hello world!\n");
+    consoleLogHex(a.x);
+    consoleLogHex(a.y);
+    consoleLogHex(a.width);
+    consoleLogHex(a.height);
+    
+    consoleLogHex(b.x);
+    consoleLogHex(b.y);
+    consoleLogHex(b.width);
+    consoleLogHex(b.height);
+    
 }
--- test/test_sprites.py
+++ test/test_sprites.py
@@ -1,2 +1,19 @@
+import durango_testing
+
 def test_answer():
-    assert 3+2 == 5
+    internal_cols(0,0,50,50)
+
+def internal_cols(x1, y1, x2, y2):
+    data = bytearray(8192)
+    data[0]=x1
+    data[1]=y1
+    data[2]=10
+    data[3]=10
+    data[4]=x2
+    data[5]=y2
+    data[6]=10
+    data[7]=10
+    
+    dump = durango_testing.run_durango('test_sprites', data)
+
+    assert dump[0x6000] == 0x00
